fcd4300ab18d8c9b116747a99cf05886c5c56331
Adossi Fred William <f.adossi@alustudent.com>
2025-06-19 20:51:22 +0200

    momo_analysis.py

--- momo_analysis.py
+++ momo_analysis.py
@@ -1,21 +1,24 @@
-#!/usr/bin/env python3
 from backend.api import create_app
 from waitress import serve
-import logging
 import argparse
-
-app = create_app()
+import logging
 
 def parse_args():
     parser = argparse.ArgumentParser(description='MTN MoMo Transaction Analysis System')
-    parser.add_argument('--port', type=int, default=5000, help='Port to run the server on')
-    parser.add_argument('--host', default='0.0.0.0', help='Host to bind the server to')
-    parser.add_argument('--debug', action='store_true', help='Run in debug mode')
+    parser.add_argument('--port', type=int, default=5000)
+    parser.add_argument('--host', default='0.0.0.0')
+    parser.add_argument('--debug', action='store_true')
     return parser.parse_args()
 
 def main():
     args = parse_args()
+    app = create_app()
     
+    logging.basicConfig(
+        level=logging.DEBUG if args.debug else logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
     if args.debug:
         logging.info("Running in debug mode")
         app.run(debug=True, host=args.host, port=args.port)
@@ -24,5 +27,4 @@ def main():
         serve(app, host=args.host, port=args.port)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     main()
