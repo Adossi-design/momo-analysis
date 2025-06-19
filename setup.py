0f059fa539d94802a5734fd25b7e3d3dd69c8cd5
Adossi Fred William <f.adossi@alustudent.com>
2025-06-20 00:13:44 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,4 @@ setup(
     name="momo_analysis",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        'Flask>=2.0.0',
-        'waitress>=2.0.0',
-        'lxml>=4.0.0'
-    ],
-    python_requires='>=3.8',
-    entry_points={
-        'console_scripts': [
-            'momo-analysis=momo_analysis:main',
-        ],
-    },
 )
