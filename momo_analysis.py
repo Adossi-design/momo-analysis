c66a0822c0a2db009ab67a9aebea7ff75554d878
Adossi Fred William <f.adossi@alustudent.com>
2025-06-19 18:16:30 +0200

    momo_analysis.py

--- momo_analysis.py
+++ momo_analysis.py
@@ -1,11 +1,12 @@
-from backend.api import app
+#!/usr/bin/env python3
+from backend.api import create_app
 from waitress import serve
 import logging
 import argparse
 
-logging.basicConfig(level=logging.INFO)
+app = create_app()
 
-def parse_arguments():
+def parse_args():
     parser = argparse.ArgumentParser(description='MTN MoMo Transaction Analysis System')
     parser.add_argument('--port', type=int, default=5000, help='Port to run the server on')
     parser.add_argument('--host', default='0.0.0.0', help='Host to bind the server to')
@@ -13,7 +14,7 @@ def parse_arguments():
     return parser.parse_args()
 
 def main():
-    args = parse_arguments()
+    args = parse_args()
     
     if args.debug:
         logging.info("Running in debug mode")
@@ -23,4 +24,5 @@ def main():
         serve(app, host=args.host, port=args.port)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     main()
